118c98e270b1d5c762dc00f4bb444999f814c9ac
Philip Tolentino <philipm.tolentino@gmail.com>
2024-11-04 02:29:19 +0800

    nav fix

--- components/header.tsx
+++ components/header.tsx
@@ -33,6 +33,7 @@ export const Header = () => {
   const pathname = usePathname();
   const { setTheme, theme, systemTheme } = useTheme();
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
   const supabase = createClient();
 
@@ -54,8 +55,8 @@ export const Header = () => {
 
   return (
     <header className="sticky top-0 z-10 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-      <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-        <Link href="/" className="flex items-center">
+      <div className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6 p-4">
+        <Link href="/user" className="flex text-lg font-semibold md:text-base">
           {theme === "dark" ||
           (theme === "system" && systemTheme === "dark") ? (
             <Image
@@ -63,7 +64,7 @@ export const Header = () => {
               alt="Chicken Near Me Logo"
               height={48}
               width={160}
-              className="h-auto max-w-[150px] object-contain"
+              className="h-10 w-auto"
               priority
             />
           ) : (
@@ -72,7 +73,7 @@ export const Header = () => {
               alt="Chicken Near Me Logo"
               height={48}
               width={160}
-              className="h-auto max-w-[150px] object-contain"
+              className="h-10 w-auto object-contain"
               priority
             />
           )}
@@ -91,8 +92,9 @@ export const Header = () => {
             {label}
           </Link>
         ))}
-      </nav>
-      <Sheet>
+      </div>
+
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="shrink-0 md:hidden">
             <Menu className="h-5 w-5" />
@@ -101,7 +103,11 @@ export const Header = () => {
         </SheetTrigger>
         <SheetContent side="left">
           <nav className="grid gap-6 text-lg font-medium">
-            <Link href="/" className="relative flex items-center right-8">
+            <Link
+              href="/"
+              className="flex items-center gap-2 text-lg font-semibold"
+              onClick={() => setIsOpen(false)}
+            >
               {theme === "dark" ||
               (theme === "system" && systemTheme === "dark") ? (
                 <Image
@@ -109,7 +115,7 @@ export const Header = () => {
                   alt="Chicken Near Me Logo"
                   height={48}
                   width={160}
-                  className="h-auto max-w-[150px] object-contain"
+                  className="h-10 md:h-14 lg:h-16 w-auto"
                   priority
                 />
               ) : (
@@ -118,7 +124,7 @@ export const Header = () => {
                   alt="Chicken Near Me Logo"
                   height={48}
                   width={160}
-                  className="h-auto max-w-[150px] object-contain"
+                  className="h-10 w-auto object-contain"
                   priority
                 />
               )}
@@ -127,9 +133,13 @@ export const Header = () => {
               <Link
                 key={href}
                 href={href}
-                className={cn("hover:text-foreground text-muted-foreground", {
-                  "text-foreground font-bold": pathname === href,
-                })}
+                className={cn(
+                  "hover:text-[#c41b1b] text-muted-foreground transition-colors",
+                  {
+                    "text-[#c41b1b] font-bold": pathname === href,
+                  }
+                )}
+                onClick={() => setIsOpen(false)}
               >
                 {label}
               </Link>
